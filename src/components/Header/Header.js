cfc034bc2f023584b8bc47cd42ae099cd384582c
Sanskar Gupta <Sanskar95@users.noreply.github.com>
2020-10-07 16:17:44 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import './Header.css'
+
+
+const  Header=()=> {
+    return (
+        <div className="header">
+         <p>soldcfijsldk</p>
+        </div>
+    );
+}
+
+export default Header;
